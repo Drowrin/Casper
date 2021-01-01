794f261546ade7118f9ebc8fc2855d6363c24c86
Drowrin <drowrin@gmail.com>
2021-01-01 14:48:59 -0500

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,21 @@
 import express = require('express');
+import cors = require('cors');
 import { Casper } from "./casper";
 
 const casper = new Casper();
 
-if (casper.length == 0) {
-    console.error("No entities loaded! At least one entry is required");
-} else {
-    console.log(`Loaded ${casper.length} entities!`);
+console.log(`Loaded ${casper.length} entities!`);
 
-    const app = express();
+const app = express();
+app.use(cors());
 
-    app.get('/', (req, res) => {
-        res.json(casper);
-    });
+app.get('/', (req, res) => {
+    res.json(casper);
+});
 
-    app.get('/entity/:id', (req, res) => {
-        res.json(casper.get(req.params.id));
-    });
-    
-    app.listen(3000);
-    console.log('Express started on port 3000');
-}
+app.get('/entity/:id', (req, res) => {
+    res.json(casper.get(req.params.id));
+});
+
+app.listen(3001);
+console.log('Express started on port 3001');
