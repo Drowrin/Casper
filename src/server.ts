004b097029ab0716675435e20ff44b4ca18e826c
Drowrin <drowrin@gmail.com>
2021-01-01 18:14:11 -0500

    add version hash endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express = require('express');
 import cors = require('cors');
+import hash = require('object-hash');
 import { Casper } from "./casper";
 
 const casper = new Casper();
 
 console.log(`Loaded ${casper.length} entities!`);
+
+const casperHash = hash(casper);
+
+console.log(`Casper version hash: ${casperHash}`);
 
 const app = express();
 app.use(cors());
@@ -15,6 +20,10 @@ app.get('/', (req, res) => {
 
 app.get('/entity/:id', (req, res) => {
     res.json(casper.get(req.params.id));
+});
+
+app.get('/hash', (req, res) => {
+    res.send(casperHash);
 });
 
 app.listen(3001);
