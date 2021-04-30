3bf8bae9a8ad1b0e4cbdf7809ab2946abad33fcb
Drowrin <drowrin@gmail.com>
2021-04-30 16:36:15 -0400

    add markdown rendering to description

--- src/schema/description.ts
+++ src/schema/description.ts
@@ -1,3 +1,5 @@
+import { Converter } from 'showdown';
+import { Entity, Manifest } from '.';
 import { component } from './component';
 
 export type DescriptionData = string;
@@ -5,8 +7,10 @@ export type DescriptionData = string;
 @component('description')
 export class Description {
     raw: string;
+    rendered: string;
 
-    constructor(data: DescriptionData) {
+    constructor(data: DescriptionData, _e: Entity, _m: Manifest, c: Converter) {
         this.raw = data;
+        this.rendered = c.makeHtml(this.raw);
     }
 }
