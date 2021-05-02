1a05ff9c78b51a51b4be0c6ca8fd18a2e57bf64e
Drowrin <drowrin@gmail.com>
2021-05-02 13:20:43 -0400

    make Component more extensible

--- src/schema/component.ts
+++ src/schema/component.ts
@@ -67,9 +67,22 @@ export namespace Component {
      * @param {Component.Context} ctx The context of resolving this entity
      */
     export function resolve(c: Component, ctx: Context) {
-        // only operate if the trigger passes or the KEY exists on the data
-        if (c.trigger?.(ctx) || c.KEY in ctx.data) {
-            const cData = (<any>ctx.data)[c.KEY];
+        // if the component doesn't have a trigger function, default to checking for KEY
+        let trigger = c.trigger || ((_ctx) => c.KEY in _ctx.data);
+
+        // if the component doesn't have a getData function, default to EntityData[KEY]
+        let getData = c.getData || ((_ctx) => (<any>_ctx.data)[c.KEY]);
+
+        // if the component doesn't have a process function, default to a passthrough
+        let process = c.process || ((data, _) => data);
+
+        // if the component doesn't have a transform function, default to placing at parent[KEY]
+        let transform = c.transform || ((o, _ctx) => (_ctx.parent[c.KEY] = o));
+
+        // only operate if the trigger passes
+        if (trigger(ctx)) {
+            // get component-specific data from the entity data
+            const cData = getData(ctx);
 
             // check that the parent entity contains all of the other components required by this component
             c.REQUIRES?.forEach((r) => {
@@ -77,8 +90,10 @@ export namespace Component {
                     throw `${ctx.parent.id} does not contain "${r}", which is a requirement for "${c.KEY}"`;
             });
 
-            let out = c.process?.(cData, ctx) || cData;
-            if (out !== undefined) ctx.parent[c.KEY] = out;
+            let out = process(cData, ctx);
+
+            // only assign data to parent if the output is defined
+            if (out !== undefined) transform(out, ctx);
         }
     }
 }
@@ -99,13 +114,31 @@ export interface Component {
     /**
      * A function to determine if this component's processing should trigger.
      * If this function is not included, the default trigger checks if KEY is in the data.
+     * @param {Component.Context} ctx The context of this processing call. See Component.Context for more info
+     * @returns {boolean} Will be true only if the component's processing should run
      */
     trigger?(ctx: Component.Context): boolean;
 
     /**
+     * A function that gets the component data from the entity data after the trigger passes.
+     * If this function is not included, the default behavior is to read EntityData[KEY].
+     * @param {Component.Context} ctx The context of this processing call. See Component.Context for more info
+     * @returns {any} The data to be passed to the process function
+     */
+    getData?(ctx: Component.Context): any;
+
+    /**
+     * A function that transforms the parent entity using the processed data.
+     * If this function is not included, the default behavior is to enter data at parent[KEY].
+     * @param {any} processed The processed data
+     * @param {Component.Context} ctx The context of this processing call. See Component.Context for more info
+     */
+    transform?(processed: any, ctx: Component.Context): void;
+
+    /**
      * A function that is called to process input data before it is entered into the final manifest.
-     *
      * This function is optional. If not present, the data will be copied from source as-is.
+     * If the function does not return or returns `undefined`, the transform function will not be triggered.
      *
      * @param {any} data The data for this component, parsed directly from the source yaml
      * @param {Component.Context} ctx The context of this processing call. See Component.Context for more info
