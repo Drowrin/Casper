2843f08ceb2eeb7bab4ad74c76ce4b3dca0e0b77
Drowrin <drowrin@gmail.com>
2021-05-17 11:16:55 -0400

    allow '-' in id references

--- src/markdown.ts
+++ src/markdown.ts
@@ -2,9 +2,9 @@ export function casperMarkdown(m: { [key: string]: any }) {
     return function () {
         let entitylink = {
             type: 'lang',
-            regex: /\B@([\w.]+\w)/g,
+            regex: /\B@([\w.-]+\w)/g,
             replace: function (_: string, id: string) {
-                let name = m[id]?.name || `[Reference Error: ${id}]`;
+                let name = m[id]?.name || id;
                 let path = id.replace('.', '/');
                 return `<a href="/${path}" class="markdown-entity-link" data-entity-id="${id}">${name}</a>`;
             },
