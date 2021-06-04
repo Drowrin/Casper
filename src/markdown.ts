c37e1d816e745412d12801d9cac3718b2d4edd1d
drowrin <drowrin@gmail.com>
2021-06-04 14:34:43 -0400

    more accurate id regex

--- src/markdown.ts
+++ src/markdown.ts
@@ -2,7 +2,7 @@ export function casperMarkdown(m: { [key: string]: any }) {
     return function () {
         let entitylink = {
             type: 'lang',
-            regex: /\B@([\w.-]+\w)/g,
+            regex: /\B@([\w\.\-]+[\w\*])/g,
             replace: function (_: string, id: string) {
                 let name = m[id]?.name || id;
                 let path = id.replace('.', '/');
